658cbd4d4379e2055686c244122b34d424528fc6
Kevin Chandra <kacol.bot@gmail.com>
2024-12-30 15:42:35 +0700

    whatsapp share

--- js/app.js
+++ js/app.js
@@ -208,6 +208,118 @@ document.addEventListener('alpine:init', function() {
         });
         // this.calculateSum();
         this.$dispatch('participant-cleared');
+      },
+      shareBill() {
+        let messages = [];
+        this.participants.forEach((participant, index) => {
+          messages.push(this.formatForWhatsapp(index).join('\n'));
+          if (index < this.participants.length - 1) {
+            messages.push([]);
+          }
+        });
+
+        return this.constructWhatsappUrl(messages);
+      },
+      shareParticipant(index) {
+        const message = this.formatForWhatsapp(index);
+
+        return this.constructWhatsappUrl(message);
+      },
+      constructWhatsappUrl(messages) {
+        window.open(
+            'https://wa.me/?text=' +
+            encodeURI('```' + messages.join('\n') + '```'),
+            '_blank'
+        );
+      },
+      formatForWhatsapp(index) {
+        let participant = this.participants[index];
+        const chars = {
+          'col': 5,
+          'line': 30
+        };
+        const Col = function(columns, content, align, pad) {
+          if (columns === 'auto') {
+            return content;
+          }
+
+          if (columns === 'full') {
+            columns = 6;
+          }
+          if (typeof align === 'undefined') {
+            align = 'left';
+          }
+          if (typeof pad === 'undefined') {
+            pad = chars['col'] * columns;
+          }
+
+          if (align === 'left') {
+            return String(content).padEnd(pad, ' ');
+          }
+
+          if (align === 'right') {
+            return String(content).padStart(pad, ' ');
+          }
+
+          return _.pad(content, pad, ' ');
+        };
+        const Row = function(...cols) {
+          let lines = _.chunk(cols, 3);
+          let rows = [];
+          lines.forEach(line => {
+            let row = [];
+            line.forEach(col => row.push(col));
+
+            rows.push(row.join(''));
+          });
+
+          return rows;
+        };
+        const separator = function(numOfChars) {
+          if (typeof numOfChars === 'undefined') {
+            numOfChars = chars['line'];
+          }
+          return String('').padStart(numOfChars, '-');
+        };
+
+        let message = [];
+        message.push(Row(Col('full', participant.name, 'center')));
+        message.push(Row(Col('full', separator())));
+
+        participant.items.forEach(item => {
+          let itemName = item.name;
+          if (item.split !== '1/1') {
+            itemName += ` (${item.split})`;
+          }
+          message.push(
+              Row(
+                  Col(4, itemName),
+                  Col(2, number_format(item.price), 'right')
+              )
+          );
+        });
+
+        message.push(Row(Col('full', separator(10), 'right')));
+
+        participant.breakdown.forEach(breakdown => {
+          message.push(
+              Row(
+                  Col(4, breakdown.key, 'right'),
+                  Col(2, number_format(breakdown.value), 'right')
+              )
+          );
+        });
+
+        message.push(
+            Row(
+                Col(4, 'Total', 'right'),
+                Col(2, number_format(participant.sum), 'right')
+            )
+        );
+        message.push(Row(Col('full', separator())));
+
+        // console.log('```' + message.join('\n') + '```');
+        return message;
       }
     };
   });
